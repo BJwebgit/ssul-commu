b620c65a1704f7135088f8ff2d2aafc30955c48a
BJwebgit <BJwebgoog@gmail.com>
2020-05-13 16:22:11 +0900

    게시판 페이징 구현

--- routes/index.js
+++ routes/index.js
@@ -66,20 +66,67 @@ router.post('/board/write', function(req, res, next) {
 
 router.get('/board/:page', function(req, res, next) {
   var page = req.params.page;
+  page = parseInt(page, 10);
+  var size = 10;
+  var begin = (page-1)*size;
   if(req.session.email === undefined){
-    db.query(`select * from posts`,
-    function(err, result){
-        res.render("board", {
-        posts: result, session:false, page: page, length: result.length-1, page_num: 1
-      });
+    db.query(`select count(*) cnt from posts`,
+      function(err, result){
+        var cnt = result[0].cnt;
+        var totalPage = Math.ceil(cnt / size);
+        var pageSize = 10;
+        var startPage = Math.floor((page-1) / pageSize)*pageSize + 1;
+        var endPage = startPage + (pageSize - 1);
+        console.log(totalPage+" "+startPage+" "+endPage);
+        if(endPage > totalPage){
+          endPage = totalPage;
+        }
+        var max = cnt - ((page-1)*size);
+        db.query(`select * from posts order by createdAt desc limit ?,?`,
+          [begin, size],
+          function(err, result){
+            var datas = {
+              post : result,
+              page: page,
+              pageSize: pageSize,
+              startPage: startPage,
+              endPage: endPage,
+              totalPage: totalPage,
+              max: max,
+              session:false
+            };
+            res.render("board", datas);
+        });
     });
   }
   else{
-    db.query(`select * from posts`,
-    function(err, result){
-      res.render("board", {
-        posts: result, session:req.session, page: page, length: result.length-1, page_num: 1
-      });
+    db.query(`select count(*) cnt from posts`,
+      function(err, result){
+        var cnt = result[0].cnt;
+        var totalPage = Math.ceil(cnt / size);
+        var pageSize = 10;
+        var startPage = Math.floor((page-1) / pageSize)*pageSize + 1;
+        var endPage = startPage + (pageSize - 1);
+        console.log(totalPage+" "+startPage+" "+endPage);
+        if(endPage > totalPage){
+          endPage = totalPage;
+        }
+        var max = cnt - ((page-1)*size);
+        db.query(`select * from posts order by createdAt desc limit ?,?`,
+          [begin, size],
+          function(err, result){
+            var datas = {
+              post : result,
+              page: page,
+              pageSize: pageSize,
+              startPage: startPage,
+              endPage: endPage,
+              totalPage: totalPage,
+              max: max,
+              session:req.session
+            };
+            res.render("board", datas);
+        });
     });
   }
 });
